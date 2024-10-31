690fb4c785fe570de081968a925606b0f37796bd
Fabri09567 <fabrizzioa20@gmail.com>
2024-10-31 13:56:47 -0300

    Aplicaciones AFP 2 TDII2024

---
+++ AFP_2_TDII_2024/AFP_2_GRUPO_5_App_2.1/Drivers/API/Inc/API_GPIO.h
@@ -0,0 +1,22 @@
+/*
+ * API_GPIO.h
+ *
+ *  Created on: Oct 31, 2024
+ *      Author: Fabrizzio
+ */
+
+#ifndef API_INC_API_GPIO_H_
+#define API_INC_API_GPIO_H_
+
+#include <stdint.h>
+
+typedef uint16_t led_t;
+typedef uint32_t ms;
+
+void MX_GPIO_Init(void);
+void writeLedOn_GPIO (led_t LDx);
+void writeLedOff_GPIO (led_t LDx);
+void toggleLed_GPIO (led_t LDx);
+void delay_ms (uint32_t ms);
+
+#endif /* API_INC_API_GPIO_H_ */
